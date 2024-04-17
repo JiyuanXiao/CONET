a58adfbaa652834167aeff51b1dc7022a77d91fe
Ian <ian@g.ucla.edu>
2024-04-16 22:21:49 -0700

    Create MessageBubble component
    Create MessageBubble component. Message bubble is used for containing
    and display messages.
    
    react-native-size-matter is installed and used in this commit.

--- app/chat-window.tsx
+++ app/chat-window.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Text, View } from "@/components/Themed";
 import { useLocalSearchParams } from "expo-router";
 
+import MessageBubble from "@/components/MessageBubble";
+
 export default function ChatWindowScreen() {
   const { name } = useLocalSearchParams<{ name: string }>();
+  let message_1 =
+    "When developing with react-native, you need to manually adjust your app to look great on a variety of different screen sizes. That's a tedious job. react-native-size-matters provides some simple tooling to make your scaling a whole lot easier.";
+  let message_2 = "Good idea ";
   return (
     <View>
-      <Text>{name}</Text>
+      <MessageBubble message_content={message_1} />
+      <MessageBubble message_content={message_2} isReceived />
     </View>
   );
 }
