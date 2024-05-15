329f27a04710483d41630f378793ad38fc00ef1b
Ian <ian@g.ucla.edu>
2024-05-15 11:01:15 -0700

    Enable splash screen auto hide
    The testing app download from TestFlight stuck in the splash screen
    for unknown reason.
    
    Comment out the preventAutoHideAsync() funtion call to see if the
    issue fixed or not.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,7 @@ import "react-native-reanimated";
 // };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
