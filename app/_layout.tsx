f44b60315f47eea1ee78cfba035ab76695f38a96
Ian <ian@g.ucla.edu>
2024-05-16 14:16:09 -0700

    Try to uninstall expo-splash-screen to fix splashScreen stuck issue
    Uninstalled expo-splash-screen and build and submit app to apple
    store connect. SplashScreen stucking issue unsolved.
    
    expo-splash-screen is re-installed.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,19 +35,9 @@ export default function RootLayout() {
   //   if (error) throw error;
   // }, [error]);
 
-  // useEffect(() => {
-  //   if (loaded) {
-  //     SplashScreen.hideAsync();
-  //   }
-  // }, [loaded]);
-
-  const hideSplashScreen = async () => {
-    await SplashScreen.hideAsync();
-  };
-
   useEffect(() => {
-    hideSplashScreen();
-  });
+    SplashScreen.hideAsync();
+  }, []);
 
   // if (!loaded) {
   //   return null;
